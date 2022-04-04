d042c8985e74b1e6e38ef31b85f51bc72320a226
Spatika Ganesh <spatsi.bluediamond@gmail.com>
2022-04-03 19:44:56 -0700

    Updates

--- iPerceive-master/tools/hdf5_2_bufile.py
+++
@@ -1,8 +0,0 @@
-import h5py
-import numpy as np
-
-#for i in range(8):
-file = h5py.File('/data4/tingjia/wt/budata/coco_vc_xy_openimage_new.hdf5', 'r')
-for keys in file:
-    feature = file[keys]['feature'][:]
-    np.save('/data4/tingjia/wt/budata/coco_vc_xy_openimage_new/'+keys+'.npy', feature)
